eaf358415ba2eae7c13c526f8a9d3ffb0702ea78
Rista Shrestha <ristashrestha10@gmail.com>
2023-11-06 11:04:52 +0545

    profile page added

---
+++ src/components/recentlyUpdated/recentlyUpdated.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './recentlyUpdated.css'
+
+const RecentlyUpadatedStory = () => {
+    return (
+        <div className='recently-updated'>
+            <div className="image">
+                <img src="https://media.licdn.com/dms/image/D4D12AQGpFki-giIw6w/article-cover_image-shrink_720_1280/0/1686192883489?e=2147483647&v=beta&t=i9M2DSpJQFkmo_ORm02_HOgvQk6f_B-5uOeryLXVbKQ" alt="" />
+            </div>
+            <div className="text">
+                <h5 className="title">The Enchanted Forest</h5>
+                <p className="date">2023-07-09</p>
+                <p className="description">
+                    Once upon a time, in a small village nestled deep within a lush valley, there was a legend of an enchanted forest. The villagers spoke in hushed tones about the wonders and mysteries that awaited those who dared to enter. The forest was said to be a place of magic and wonder, where the trees whispered ancient secrets and the animals could speak with humans.....
+                </p>
+            </div>
+
+        </div>
+    )
+}
+
+export default RecentlyUpadatedStory
--- src/pages/profilePage/profilePage.jsx
+++ src/pages/profilePage/profilePage.jsx
@@ -3,23 +3,36 @@ import './profilePage.css'
 import { Card, Container, Row, Col } from 'react-bootstrap'
 import NavBar from '../../components/navBar/navBar'
 import Footer from '../../components/footer/footer'
+import ProfileCard from '../../components/profileCard/profileCard'
+import RecentlyUpadatedStory from '../../components/recentlyUpdated/recentlyUpdated'
 
 const ProfilePage = () => {
     return (
         <div>
             <NavBar />
             <main>
-                <div className='profile-container'>
+                <div className='container'>
                     <section className='profile-left-content'>
-                        <h2>jdfskfjs</h2>
+                        <ProfileCard />
+                        <div className='about-me-container'>
+                            <h3 className='about-text-bold'>About me</h3>
+                            <p className='about-text-light'>
+                                Passionate wordsmith who thrives on crafting stories and finding inspiration in the pages of books. Words are my world, and through them, I explore, create, and connect with the stories that make life extraordinary.
+                            </p>
+                        </div>
                     </section>
                     <section className='profile-right-content'>
-<h3>jksfahfkdjf</h3>
+                            <h3 className='recently-updated-title'>Recently Updated Stories</h3>
+                            <RecentlyUpadatedStory/>
+                            <RecentlyUpadatedStory/>
+                            <RecentlyUpadatedStory/>
+                            <RecentlyUpadatedStory/>
                     </section>
                 </div>
             </main>
             <Footer />
         </div>
+
 
     )
 }
