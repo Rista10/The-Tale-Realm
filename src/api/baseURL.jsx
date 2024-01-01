6575db8f453ff7cd190bac1b4b36bd66123c2bc5
Shashinoor13 <shashinoorghimire13@gmail.com>
2024-01-01 19:22:39 +0545

    Deploying frontend

--- src/api/baseURL.jsx
+++ src/api/baseURL.jsx
@@ -1,5 +1,6 @@
-const baseURL = 'http://localhost:5000'
+// const baseURL = 'http://localhost:5000'
 // const  baseURL='https://tale-realmm.onrender.com'
+const baseURL ="https://backend-the-tale-realm-7b8q.vercel.app"
 
 
 export default baseURL;
